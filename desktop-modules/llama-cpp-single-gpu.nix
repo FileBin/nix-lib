4f0091842e50beae9bdbaaa58aeb545efa0f3552
filebin <alex.mousse9@gmail.com>
2026-06-14 02:18:48 +0400

    added nix mcp for llama

--- desktop-modules/llama-cpp-single-gpu.nix
+++ desktop-modules/llama-cpp-single-gpu.nix
@@ -1,15 +1,23 @@
-{
+{ pkgs, ... }: {
+  # There are some MCPs
+  environment.systemPackages = [ pkgs.mcp-nixos ];
+
   services.llama-cpp = {
     enable = true;
     port = 11433;
     modelsDir = "/srv/models";
 
     extraFlags = [
-      "--parallel" "1"
-      "--flash-attn" "on"
-      "--cache-type-k" "q8_0"
-      "--cache-type-v" "q8_0"
-      "--models-max" "1"
+      "--parallel"
+      "1"
+      "--flash-attn"
+      "on"
+      "--cache-type-k"
+      "q8_0"
+      "--cache-type-v"
+      "q8_0"
+      "--models-max"
+      "1"
     ];
   };
 }
