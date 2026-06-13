c97c0c71d4845d44b95d922e1f455b4e9cae668f
filebin <alex.mousse9@gmail.com>
2026-06-14 03:09:12 +0400

    fix llama port and script

--- desktop-modules/llama-unload.nix
+++ desktop-modules/llama-unload.nix
@@ -1,18 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let 
+  llama-port = config.services.llama-cpp.port;
+  llama-apiBase = "http://localhost:${toString llama-port}";
   llama-unload = pkgs.writeShellApplication {
     name = "llama-unload";
     runtimeInputs = [
       pkgs.jq
     ];
     text = ''
-      #!/usr/bin/env bash
       # Fetch all currently loaded models, filter for active ones, and unload them one by one
-      curl -s http://localhost:8080/v1/models | \
-      jq -r '.data[] | select(.status == "loaded") | .id' | \
+      curl -s ${llama-apiBase}/v1/models | \
+      jq -r '.data[] | select(.status.value == "loaded") | .id' | \
       while read -r model; do
           echo "Unloading: $model"
-          curl -X POST "http://localhost:8080/models/unload" -H "Content-Type: application/json" -d "{\"model\": \"$model\"}"
+          curl -X POST "${llama-apiBase}/models/unload" -H "Content-Type: application/json" -d "{\"model\": \"$model\"}"
       done
     '';
   };
