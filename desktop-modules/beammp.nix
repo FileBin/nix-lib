cbbbd523c5f5e06ac5f4a2d917f895b12452a84e
filebin <alex.mousse9@gmail.com>
2026-06-28 10:09:42 +0400

    added beammp

---
+++ desktop-modules/beammp.nix
@@ -0,0 +1,63 @@
+{ pkgs, lib, config, ... }:
+{
+  options.gaming.beammp.enable = lib.mkEnableOption "BeamMP Launcher";
+  config = lib.mkIf config.gaming.beammp.enable {
+    environment.systemPackages = 
+    let
+      beammpIcon = pkgs.fetchurl {
+        url = "https://beammp.com/assets/BeamMP_blk-BycyukAv.png";
+        sha256 = "sha256-7osWdvH3vG6Jf2I9U0/OPPVSL8wDlTIenwytng5xOyM=";
+      };
+
+
+      launcher = pkgs.unstable.buildFHSEnv {
+        name = "BeamMP-Launcher";
+        targetPkgs = pkgs: with pkgs; [
+          nspr
+          libuuid
+          fontconfig
+          freetype
+          glib
+          nss
+          dbus
+          at-spi2-atk
+          cups
+          libx11
+          libxcomposite
+          libxdamage
+          libxext
+          libxfixes
+          libxrandr
+          libxcb
+          libxkbcommon
+          cairo
+          pango
+          udev
+          alsa-lib
+          libgbm
+
+
+          libGL
+          libGLU
+          vulkan-loader
+          vulkan-tools
+          libvdpau
+          libva
+        ];
+        runScript = "${pkgs.unstable.beammp-launcher}/bin/BeamMP-Launcher";
+      };
+
+
+      desktopItem = pkgs.makeDesktopItem {
+        name = "BeamMP-Launcher";
+        desktopName = "BeamMP";
+        comment = "BeamNG.drive Multiplayer";
+        exec = "${launcher}/bin/BeamMP-Launcher";
+        icon = "${beammpIcon}";
+        categories = [ "Game" ];
+        terminal = true;
+      };
+    in
+    [ launcher desktopItem ];
+  };
+}
